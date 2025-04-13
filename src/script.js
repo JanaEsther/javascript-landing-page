c1898779bdb44141b611801257a61477c0783f8b
JanaEsther <j.obendraufova@h2o-logic.cz>
2025-04-13 12:59:14 +0200

    desktop_hide

--- src/script.js
+++ src/script.js
@@ -1,29 +1,45 @@
 const usersTable = [
   // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
-  { username: "hello@world.com" },
-  { username: "test@user.com" },
-  { username: "email@domain.com" },
+  { username: 'hello@world.com' },
+  { username: 'test@user.com' },
+  { username: 'email@domain.com' },
 ];
 let renderSuccess = () => {
-  document.getElementById("success-message").hidden = false;
+  document.getElementById('success-message').hidden = false;
 };
 let renderEmailTakenError = () => {
-  document.getElementById("taken-error-message").hidden = false;
+  document.getElementById('taken-error-message').hidden = false;
 };
 let renderEmailEmptyError = () => {
-  document.getElementById("empty-error-message").hidden = false;
+  document.getElementById('empty-error-message').hidden = false;
 };
 let resetMessage = () => {
-  document.getElementById("success-message").hidden = true;
-  document.getElementById("taken-error-message").hidden = true;
-  document.getElementById("empty-error-message").hidden = true;
+  document.getElementById('success-message').hidden = true;
+  document.getElementById('taken-error-message').hidden = true;
+  document.getElementById('empty-error-message').hidden = true;
 };
 
-addEventListener("submit", (event) => {
+addEventListener('submit', (event) => {
   event.preventDefault();
   resetMessage();
 
-  let email = document.getElementById("email").value;
+  let email = document.getElementById('email').value;
+
+  let isEmailTaken = usersTable.some((user) => user.username === email);
+
+  let lastEmail;
+
+  let isEmailRepeated = email === lastEmail;
+
+  if (isEmailTaken) {
+    return 'taken-error-message';
+  } else if (isEmailRepeated) {
+    return 'Repeated ';
+  } else if (email === '') {
+    return 'empty-error-message';
+  } else {
+    return 'success message';
+  }
 
   // TODO: Show Correct Status Messages on Signup Form
   // 1. successful signup
@@ -33,10 +49,6 @@ addEventListener("submit", (event) => {
 });
 
 let toggleNav = () => {
-  var nav = document.getElementById("mobile-nav");
-  if (nav.className.indexOf("show") == -1) {
-    nav.className += " show";
-  } else {
-    nav.className = nav.className.replace(" show", "");
-  }
+  let nav = document.getElementById('mobile-nav');
+  nav.classList.toggle('show');
 };
